1867d4ff55f50150588f5546443cba03478ad5ab
Wenyin Wei <wenyin.wei.ww@gmail.com>
2021-05-09 19:14:45 +0800

    upgrade to 0.1.1 version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="sinupy",
-    version="0.0.1",
+    version="0.1.1",
     author="Wenyin Wei",
     author_email="wenyin.wei@ipp.ac.cn",
     description="The sinupy package make it convenient to analyze the characteristic sinusoidal waves propagating in various kinds of medium. Typical waves in plasma have been given in the code to demonstrate how the package works and other users are free to follow the steps to research on their medium or charcteristic waves of interest.",
