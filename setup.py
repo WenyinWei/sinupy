9ba3664f50c2d1942a60778a7647819e035ca8d3
Wenyin Wei <wenyin.wei.ww@gmail.com>
2021-05-11 17:09:17 +0800

    fix grammar typos

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setuptools.setup(
     version="0.1.1",
     author="Wenyin Wei",
     author_email="wenyin.wei@ipp.ac.cn",
-    description="The sinupy package make it convenient to analyze the characteristic sinusoidal waves propagating in various kinds of medium. Typical waves in plasma have been given in the code to demonstrate how the package works and other users are free to follow the steps to research on their medium or charcteristic waves of interest.",
+    description="The sinupy package makes it convenient to analyze the characteristic sinusoidal waves propagating in various kinds of medium. Typical waves in plasma have been given in the code to demonstrate how the package works and other users are free to follow the steps to research on their medium or charcteristic waves of interest.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/WenyinWei/sinupy",
